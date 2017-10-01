de8730daace09a9718cbfddb9e6bbdc97d1df816
AIOSecurity <AIOSecurity@pi.com>
2017-10-01 08:51:37 +0000

    Mac to db

---
+++ dbconnector.php
@@ -0,0 +1,4 @@
+<?php
+    $pdo = new PDO('mysql:host=localhost;dbname=aiosecurity', 'aiosecurity', '@10s€curitY'); /*Datenbankverbindung herstellen*/
+    $pdo->query("SET NAMES 'utf8'"); /*UTF-8*/
+?>
--- todb.php
+++ todb.php
@@ -1,3 +1,8 @@
 <?php
-echo $_POST['macaddr'];
+require('dbconnector.php');
+ $mac = $_POST['macaddr'];
+ $intmac = base_convert($mac, 16, 10); // ints are processable more easily, don´t forget to convert back!
+ $statement = $pdo->prepare('INSERT INTO `users` (mac_addr,protocol,login_time) VALUES (:mac_addr,:protocol, CURRENT_TIMESTAMP)');
+ $statement->execute(array('mac_addr' => $intmac, 'protocol' => 'sth'));
+
 ?>
